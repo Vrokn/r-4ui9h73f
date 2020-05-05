73568088a074aa2a2c71ae773dd02bae9ecb6dfc
Mateo <mgomezossa@gmail.com>
2020-05-04 20:21:14 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,15 +2,25 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  state = {
+    repetidor: '',
+  };
+
+  repetidor = (event) => {
+    this.setState({
+      repetidor: event.target.value,
+    });
+  };
+
   render() {
-    return (
-      <div>
-        <h1>Un repetidor</h1>
-        <input type="text" placeholder="Empieza a escribir algo" />
-        <p className="repeater"></p>
-      </div>
-    );
-  }
+  return (
+    <div>
+      <h1>Un repetidor</h1>
+      <input type="text" name='entrie' placeholder="Empieza a escribir algo" onChange={(event) => this.repetidor(event)} />
+      <p className="repeater">{this.state.repetidor}</p>
+    </div>
+  );
+}
 }
 
 export default App;
